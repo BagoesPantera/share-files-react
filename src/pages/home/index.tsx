ed7177950c995bb5a00eab7901b9ea6a638edde1
BagoesPantera <bagoespantera01@gmail.com>
2024-01-01 18:19:01 +0800

    style: add styling

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import {ReactElement, useRef, useState} from "react";
 import {FileUploader} from "react-drag-drop-files";
-import {Form} from "react-router-dom";
+import {Form, useActionData} from "react-router-dom";
 
 export async function action({request}): Promise<void>{
     try{
         const formData = await request.formData();
         formData.append('content', 'New file!') // guilded require this
+
+        if (formData.get("file[0]").name === ""){
+            return null
+        }
 
         const resp = await fetch(import.meta.env.VITE_WEBHOOK_URL,{
             method: "POST",
@@ -16,7 +20,7 @@ export async function action({request}): Promise<void>{
 
         // console.log(formData)
         console.log(url)
-        return null
+        return url
     }catch (e) {
         return e
     }
@@ -25,8 +29,11 @@ export async function action({request}): Promise<void>{
 const fileTypes: String[] = ["JPG", "PNG", "GIF"];
 
 export default function Home(): ReactElement {
-    const [file, setFile]: any = useState(null);
+    const [file, setFile]: any = useState(null)
+    const [preview, setPreview] = useState(null)
     const form = useRef(null)
+
+    const url = useActionData()
     const handleChange = (file): void => {
         setFile(file);
         // form.current.submit()
@@ -35,11 +42,15 @@ export default function Home(): ReactElement {
 
     return (
         <>
-            <Form method={"POST"} encType={"multipart/form-data"} ref={form}>
-                <FileUploader handleChange={handleChange} name="file[0]" types={fileTypes} required={true} />
-                <button type="submit">klik</button>
-            </Form>
-            <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+            <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+                <div className="align-items-center justify-content-center text-center">
+                    <Form method={"POST"} encType={"multipart/form-data"} ref={form}>
+                        <FileUploader handleChange={handleChange} name="file[0]" types={fileTypes} required={false}/>
+                        <p className={"border border-primary mt-4 border-opacity-50 p-2"}>{file ? `File name: ${file.name}` : "no files selected yet"}</p>
+                        <button type="submit" className="btn btn-light mt-2">Get url!</button>
+                    </Form>
+                </div>
+            </div>
         </>
     )
 }
