14c4083dcdc3f0d5ad1cda54be0b81b2ec5a4d63
BagoesPantera <bagoespantera01@gmail.com>
2024-01-01 18:58:49 +0800

    feat: add copy to clipboard

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import {ReactElement, useRef, useState} from "react";
 import {FileUploader} from "react-drag-drop-files";
 import {Form, useActionData} from "react-router-dom";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export async function action({request}): Promise<void>{
     try{
@@ -29,7 +30,8 @@ export async function action({request}): Promise<void>{
 const fileTypes: String[] = ["JPG", "PNG", "GIF"];
 
 export default function Home(): ReactElement {
-    const [file, setFile]: any = useState(null)
+    const [file, setFile] = useState<any>(null)
+    const [copied, setCopied] = useState<boolean>(false)
     const [preview, setPreview] = useState(null)
     const form = useRef(null)
 
@@ -43,12 +45,28 @@ export default function Home(): ReactElement {
     return (
         <>
             <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-                <div className="align-items-center justify-content-center text-center">
+                <div className="align-items-center justify-content-center text-center w-50">
                     <Form method={"POST"} encType={"multipart/form-data"} ref={form}>
                         <FileUploader handleChange={handleChange} name="file[0]" types={fileTypes} required={false}/>
                         <p className={"border border-primary mt-4 border-opacity-50 p-2"}>{file ? `File name: ${file.name}` : "no files selected yet"}</p>
                         <button type="submit" className="btn btn-light mt-2">Get url!</button>
                     </Form>
+                    {
+                        url ?
+                            <CopyToClipboard text={url as string} onCopy={() => setCopied(true)}>
+                                <span className={"user-select-all text-wrap"} role={"button"}>{url}</span>
+                            </CopyToClipboard>
+                            :
+                            null
+                    }
+                    <div className="mt-4">
+                        <CopyToClipboard text={url as string} onCopy={() => setCopied(true)}>
+                            <p className={"user-select-all text-nowrap"} role={"button"}>https://s3-us-west-2.amazonaws.com/www.guilded.gg/WebhookPrimaryMedia/1e28488dbb935c069ab45de0fa97c376-Full.webp</p>
+                        </CopyToClipboard>
+                    </div>
+
+
+                    {copied ? <p className={"text-success mt-2"}>Copied.</p> : null}
                 </div>
             </div>
         </>
